12f61acfae4a5ea20dc6d39bd563aa8e647b9394
Sato Kazuma <sato.kazuma.kzs@gmail.com>
2016-07-11 08:37:37 +0900

    extract ngdp_d

--- economic_charts.py
+++ economic_charts.py
@@ -10,18 +10,26 @@ class WeoSubject:
 
 class Val:
     def __init__(self, year, val):
-        ""
+        self.year =  year
+        self.val = val
 
 def fetch(subject_code, line):        
-    return list(filter(lambda c: c[2] == subject, line))
+    return list(filter(lambda c: c[2] == subject_code, line))
+   
+def create_ngdp_d(line):
+    code = 'NGDP_D'
+    unit = line[6]
+    scale = line[7]
+    return  [Val(a[1], line[a[0]])  for a in zip(range(9,46), range(1980, 2017))]
 
 if __name__ == '__main__':
     b=[] 
     ngdp_d=[]
+    ngdpd=[]
     with open('weoreptc.tsv') as f:
         a= [l.split('\t')[:-1] for l in f.readlines()]
         b=list(filter(lambda x: x[9] != '', a))
-        ngdp_d = list(filter(lambda x: x[2] == 'NGDP_D', b))
-#        for r in f.readlines(:
-#            print(len(r.split('\t')[:-1]))
+        ngdp_d = fetch('NGDP_D', b)
+        ngdpd = fetch('NGDPD', b)
+#        ngdp_d = list(filter(lambda x: x[2] == 'NGDP_D', b))
 
