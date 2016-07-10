a3edcd326e2a9f51cd425cc3ec487e626c400f79
Sato Kazuma <sato.kazuma.kzs@gmail.com>
2016-07-11 08:44:30 +0900

    set properties

--- economic_charts.py
+++ economic_charts.py
@@ -6,7 +6,10 @@ class Country:
         ":"
 class WeoSubject: 
     def __init__(self, code, y_val, unit, scale):
-        ""
+        self.code = code
+        self.y_val = y_val
+        self.unit = unit
+        self.scale = scale
 
 class Val:
     def __init__(self, year, val):
@@ -20,12 +23,13 @@ def create_ngdp_d(line):
     code = 'NGDP_D'
     unit = line[6]
     scale = line[7]
-    return  [Val(a[1], line[a[0]])  for a in zip(range(9,46), range(1980, 2017))]
+    return WeoSubject(code, [Val(a[1], line[a[0]])  for a in zip(range(9,46), range(1980, 2017))], unit, scale) 
 
 if __name__ == '__main__':
     b=[] 
     ngdp_d=[]
     ngdpd=[]
+    ngdpd_sub = [create_ngdp_d(a) for a  in  ngdp_d]
     with open('weoreptc.tsv') as f:
         a= [l.split('\t')[:-1] for l in f.readlines()]
         b=list(filter(lambda x: x[9] != '', a))
