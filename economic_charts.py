d86bf50af393c3ae2e59ebb8f5f02069e28deb08
Sato Kazuma <sato.kazuma.kzs@gmail.com>
2016-07-11 21:16:24 +0900

    instantiate country

--- economic_charts.py
+++ economic_charts.py
@@ -3,7 +3,9 @@
 
 class Country:
     def __init__(self, country, weo_subjects):
-        ":"
+        self.country = country
+        self.weo_subjects = weo_subjects
+
 class WeoSubject: 
     def __init__(self, code, y_val, unit, scale):
         self.code = code
@@ -18,7 +20,13 @@ class Val:
 
 def fetch(subject_code, line):        
     return list(filter(lambda c: c[2] == subject_code, line))
-   
+
+def create_subject(line):
+    code = line[2]
+    unit = line[6]
+    scale = line[7]
+    return WeoSubject(code, [Val(a[1], line[a[0]])  for a in zip(range(9,46), range(1980, 2017))], unit, scale)   
+
 def create_ngdp_d(line):
     code = 'NGDP_D'
     unit = line[6]
@@ -26,14 +34,14 @@ def create_ngdp_d(line):
     return WeoSubject(code, [Val(a[1], line[a[0]])  for a in zip(range(9,46), range(1980, 2017))], unit, scale) 
 
 if __name__ == '__main__':
-    b=[] 
-    ngdp_d=[]
-    ngdpd=[]
-    ngdpd_sub = [create_ngdp_d(a) for a  in  ngdp_d]
-    with open('weoreptc.tsv') as f:
-        a= [l.split('\t')[:-1] for l in f.readlines()]
-        b=list(filter(lambda x: x[9] != '', a))
-        ngdp_d = fetch('NGDP_D', b)
-        ngdpd = fetch('NGDPD', b)
-#        ngdp_d = list(filter(lambda x: x[2] == 'NGDP_D', b))
 
+    with open('weoreptc.tsv') as f:
+        lines = [l.split('\t')[:-1] for l in f.readlines()]
+        filled_lines = list(filter(lambda x: x[9] != '', lines))
+        subjects = [(s[3], create_subject(s)) for s in filled_lines]
+#        ngdp_d = list(filter(lambda x: x[2] == 'NGDP_D', b))
+        countries= [a for a in set([a[0] for a in subjects])]
+
+
+        #lambda x: filter(lambda a: a == x, subjects)
+        country_subs = list(map(lambda country: Country(country, list(map(lambda x: x[1], filter(lambda s: s[0] == country, subjects)))), countries))
