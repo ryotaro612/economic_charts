1ea3fccd7598833d195d9564c897042270b605df
Sato Kazuma <sato.kazuma.kzs@gmail.com>
2016-07-13 07:52:22 +0900

    fix methods

--- economic_charts.py
+++ economic_charts.py
@@ -5,7 +5,7 @@ class Country:
     def __init__(self, country, weo_subjects):
         self.country = country
         self.weo_subjects = weo_subjects
-
+    """ 
     def create_gdp_constant_price(self, year):
         # deflator
         ngdp_d = [a for a in list(filter(lambda e: e.code == 'NGDP_D', self.weo_subjects))[0].y_val if a.year == year][0]
@@ -20,20 +20,15 @@ class Country:
         print(unit)
         print(ngdp_d.val)
         return ngdpd
-
-    def gdp_constant_usscale_calculable(year):
-        return false
-
+    """
     def val_defined(self, subject_code, year):
-        subject = get_subject(subject_code) 
-        val = [val.val for val in subject.vals if val.year == year][0]
+        val = self.get_val(subject_code, year)
         return val != '' and val != 'n/a'
-    """
-    def ngdpd_defined(self, year):
-        ngdpd = get_subject('NGDPD')
-        val = [val.val  for val in ngdpd.vals if val.year == year ][0]
-        return val != '' and val != 'n/a'
-    """
+
+    def get_val(self, subject_code, year):
+        subject = self.get_subject(subject_code)
+        return [val.val for val in subject.vals if val.year == year][0]
+
     def get_subject(self, subject_code):
         "WeoSubject"
         return list(filter(lambda subject: subject.code == subject_code, self.weo_subjects))[0]
